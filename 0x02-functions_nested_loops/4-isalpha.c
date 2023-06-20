5d7b09ef896df9278645d562f654d335fd6ff888
teezmagicfingers <teslimbobo@gmail.com>
2023-06-20 09:39:10 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: he characer to be checked
- * Return: 1 for alphabetic character or 0 for anything else 
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
